c78bb77964f4adbdeb146c193cbf7ffc2a999a64
moskit <tomek_niedzi@o2.pl>
2024-11-10 18:06:50 +0100

    simple publishing of frames works

--- setup.py
+++ setup.py
@@ -5,23 +5,14 @@ package_name = 'stereo_3d_tracking'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='Tomasz Niedzialkowski',
-    maintainer_email='tomek_niedzi@o2.pl',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': ['pytest']
-    },
+    packages=['stereo_3d_tracking'],
+    install_requires=['setuptools', 'opencv-python', 'cv-bridge'],
+    data_files=[],
     entry_points={
         'console_scripts': [
+            'image_publisher_node = stereo_3d_tracking.frame_publisher:main',
         ],
     },
+    script_dir='scripts',  # Example if you have custom scripts to install
+    install_scripts='scripts',  # Example if you are installing scripts
 )
